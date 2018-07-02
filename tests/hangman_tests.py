29f243548d1513c2cb457a07dae6f77b7b64c8b0
Oli Bishop <oliverbishop90@gmail.com>
2018-07-02 21:56:09 +0100

    test for check_win and guess

--- tests/hangman_tests.py
+++ tests/hangman_tests.py
@@ -1,7 +1,9 @@
 import pytest
 
 from hangman import validate_input
-from hangman import display_art, HANGMANART
+from hangman import display_art
+from hangman import check_win
+from hangman import guess
 
 
 def test_validate_input_number():
@@ -15,7 +17,7 @@ def test_validate_input_multiple():
 
 
 def test_validate_input_success():
-    assert validate_input("a") == True
+    assert validate_input("a") is True
 
 
 def test_display_art_lower():
@@ -25,3 +27,19 @@ def test_display_art_lower():
 
 def test_display_art_higher():
     assert display_art(8) == ValueError
+
+
+def test_guess_single():
+    assert guess(list("____"), "abcb", "a") == list("a___")
+
+
+def test_guess_multiple():
+    assert guess(list("____"), "abcb", "b") == list("_b_b")
+
+
+def test_check_win_true():
+    assert check_win("abc") is True
+
+
+def test_check_false():
+    assert check_win("_a_") is False
