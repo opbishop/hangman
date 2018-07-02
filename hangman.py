0c53fc94a394cddb9f69ca39d8b48dd2e5bbac40
opbishop <oliverbishop90@gmail.com>
2018-07-02 17:12:25 +0100

    initial commit

---
+++ hangman.py
@@ -0,0 +1,91 @@
+import random
+
+
+def main():
+    HANGMANART = {1: '''
+      +---+
+      |   |
+          |
+          |
+          |
+          |
+    =========''', 2: '''
+      +---+
+      |   |
+      O   |
+          |
+          |
+          |
+    =========''', 3: '''
+      +---+
+      |   |
+      O   |
+      |   |
+          |
+          |
+    =========''', 4: '''
+      +---+
+      |   |
+      O   |
+     /|   |
+          |
+          |
+    =========''', 5: '''
+      +---+
+      |   |
+      O   |
+     /|\  |
+          |
+          |
+    =========''', 6: '''
+      +---+
+      |   |
+      O   |
+     /|\  |
+     /    |
+          |
+    =========''', 7: '''
+      +---+
+      |   |
+      O   |
+     /|\  |
+     / \  |
+          |
+    ========='''}
+    WORDS = ["monitor", "book"]
+
+    word_to_guess = random.choice(WORDS)
+    display = ["_"] * len(word_to_guess)
+    guesses = 7
+
+    while guesses > 0:
+        print(display)
+        print("Guess a char")
+        g = input()
+
+        if g in word_to_guess:
+            display = guess(display, word_to_guess, g)
+            if check_win(display):
+                break
+        else:
+            guesses -= 1
+            print(HANGMANART[7 - guesses])
+
+    if guesses == 0:
+        print("You lose")
+    else:
+        print("You win")
+
+
+def guess(display, word_to_guess, g):
+    for x in [pos for pos, char in enumerate(word_to_guess) if char == g]:
+        display[x] = g
+    return display
+
+
+def check_win(display):
+    return "_" not in display
+
+
+if __name__ == "__main__":
+    main()
